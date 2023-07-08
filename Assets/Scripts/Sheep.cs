4e442b52d396ee52a37fdc4f450590dfc527c577
Ben Pig Chu <benpichu@gmail.com>
2023-07-08 15:22:35 +0800

    Scene setup

--- Assets/Scripts/Sheep.cs
+++ Assets/Scripts/Sheep.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Sheep : MonoBehaviour
 {
+	public new Rigidbody2D rigidbody;
+    void Awake(){
+		rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
